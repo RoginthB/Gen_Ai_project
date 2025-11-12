e76760bdfe4ed61d1a4179bb6683b16f1f188e90
Roginth B <rohinthrewa248@gmail.com>
2025-11-12 13:06:49 +0530

    Chat bot app instial

---
+++ main.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+
+def ChatBot():
+    
+    client = OpenAI(
+    #api_key="AIzaSyChnCrgpkZoGmOeiO6lyG2spWfkE10vcT8",
+    base_url="https://generativelanguage.googleapis.com/v1beta/")
+
+    print("welcome to ChatBot! Type 'exit' to quit.")
+
+    while True:
+        text = input("You: ")
+
+        if text.strip().lower()=="exit":
+            print("ChatBot: Goodbye!")
+            break
+        response = client.chat.completions.create(
+            model="gemini-2.5-flash",
+            messages=[{
+                "role": "system",
+                "content": "You are a helful assistant."
+             },{"role": "user", "content": text}
+             ]
+        )
+        print("ChatBot: ", response.choices[0].message.content)
+        print("\nIf you want to exit, type 'exit'.")
+
+
+
+if __name__ == "__main__":
+    ChatBot()
